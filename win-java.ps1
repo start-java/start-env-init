abe281868672acbcfe1bb00b70340852421afae8
RJ.Hwang <rongjihuang@gmail.com>
2018-10-11 22:27:49 +0800

    Install VSCode-win32-x64-1.28.0.zip

--- win-java.ps1
+++ win-java.ps1
@@ -78,5 +78,42 @@ if (!(Test-Path -Path env:M2_HOME)) {
 "----------end maven----------"
 
 
+# 5. vscode (zip 包内没有多余目录)
+# https://code.visualstudio.com/docs/?dv=winzip
+$vscode_url = "https://vscode.cdn.azure.cn/stable/431ef9da3cf88a7e164f9d33bf62695e07c6c2a9/VSCode-win32-x64-1.28.0.zip"
+$vscode_package = [io.path]::GetFileName($vscode_url)
+$vscode_dir = [io.path]::GetFileNameWithoutExtension($vscode_package)
+# 5.1. 下载、安装 vscode
+$vscode_home = "$install_to_dir\$vscode_dir"
+if (!(Test-Path -Path $vscode_home)) {
+  if (!(Test-Path -Path "$package_from_dir\$vscode_package")) {
+    "下载 vscode 安装包到 $package_from_dir\$vscode_package"
+    "vscode_url=$vscode_url"
+    Invoke-WebRequest -Uri $vscode_url -OutFile "$package_from_dir\$vscode_package"
+  }
+
+  "解压 vscode 包到目录 $install_to_dir\$vscode_dir"
+  Expand-Archive -Path $package_from_dir\$vscode_package -DestinationPath $install_to_dir\$vscode_dir
+} else {
+  "目录 $vscode_home 已经存在，忽略安装 vscode！"
+}
+# 5.2. 设置 vscode 环境变量
+if (!(Test-Path -Path env:VSCODE_HOME)) {
+  "设置用户环境变量 VSCODE_HOME=$vscode_home"
+  [Environment]::SetEnvironmentVariable("VSCODE_HOME", $vscode_home, "User")
+  $old_path = [Environment]::GetEnvironmentVariable("Path", "User")
+  [Environment]::SetEnvironmentVariable("Path", "$old_path;$vscode_home\bin", "User")
+} else {
+  "VSCODE_HOME 环境变量已经存在，忽略不设置！VSCODE_HOME=$env:VSCODE_HOME"
+}
+# 5.3. 创建 data 子目录（所有扩展、配置就会保存在这里）
+if (!(Test-Path -Path "$vscode_home\data")) {
+  "创建 data 子目录 $vscode_home\data （所有 vscode 扩展、配置就会保存在这里）"
+  New-Item -ItemType "directory" -Path "$vscode_home\data"
+}
+# 5.4. 添加文件资源管理器右键快捷菜单 TODO
+"----------end vscode----------"
+
+
 $current_date_time = Get-Date -format "yyyy-MM-dd HH:mm:ss"
 "$current_date_time End"
